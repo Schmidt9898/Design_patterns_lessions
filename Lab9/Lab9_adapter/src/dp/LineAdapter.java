07212ab43007d234e731502aa7848e411c633af0
SchmidtLaci (Warden) <schmidtlacus@gmail.com>
2022-11-21 13:06:52 +0100

    object adapter
    it looks like a proxy

--- Lab9/Lab9_adapter/src/dp/LineAdapter.java
+++ Lab9/Lab9_adapter/src/dp/LineAdapter.java
@@ -1,11 +1,16 @@
 package dp;
 
-public class LineAdapter extends LegacyLine implements ShapeAdapter {
+public class LineAdapter implements ShapeAdapter {
 
+	LegacyLine target;
+	LineAdapter()
+	{
+		target = new LegacyLine();
+	}
 	public void draw(int x1, int y1, int x2, int y2)
 	{
 		System.out.print("Adapter ");
-		super.draw(x1, y1, x2, y2);
+		target.draw(x1, y1, x2, y2);
 	}
 	
 }
--- Lab9/Lab9_adapter/src/dp/RectangleAdapter.java
+++ Lab9/Lab9_adapter/src/dp/RectangleAdapter.java
@@ -1,11 +1,17 @@
 package dp;
 
-public class RectangleAdapter extends LegacyRectangle implements ShapeAdapter{
+public class RectangleAdapter implements ShapeAdapter{
+
+	LegacyRectangle target;
+	RectangleAdapter()
+	{
+		target = new LegacyRectangle();
+	}
 
 	public void draw(int x1, int y1, int x2, int y2)
 	{
 		System.out.print("Adapter ");
-		super.draw(Math.min(x1, x2), Math.min(y1, y2)
+		target.draw(Math.min(x1, x2), Math.min(y1, y2)
 					, Math.abs(x2 - x1), Math.abs(y2 - y1));
 	}
 	
